ca482e71818a544d9cf826b3d273b9315f8b22c4
chaosbambi <alissamueller@outlook.de>
2018-11-16 09:28:49 +0100

    methoden zur berechnung der statistischen kennwerte

--- src/main/java/com/mycompany/imagej/Project3_PlugIn.java
+++ src/main/java/com/mycompany/imagej/Project3_PlugIn.java
@@ -10,14 +10,7 @@ public class Project3_PlugIn implements PlugInFilter {
 	final int BLACK = 0;
 	final int WHITE = 255;
 	
-    final int COLOR_MIN_DEFAULT = BLACK;
-    final int COLOR_MAX_DEFAULT = WHITE;
-    final int PERCENTAGE_DEFAULT = 1;
-    
-    int color_min, color_max;
-	int percentage;
-	
-	int[] histogram;
+
     
     @Override    
     public int setup(String args, ImagePlus im) {  
@@ -31,4 +24,56 @@ public class Project3_PlugIn implements PlugInFilter {
         int N = ip.getHeight();
         //TODO do project
     }
+
+    double[] normHistogram(int[] histogram, int M, int N) {
+    	double [] normedHist = new double[histogram.length];
+    	for(int i=0;i<histogram.length;i++) {
+    		normedHist[i] = histogram[i]/(double)(N*M);
+    	}
+    	return normedHist;
+    }
+    
+    double mean(double[] nHist) {
+    	double mean = 0;
+    	for(int i=0; i < nHist.length; i++) {
+    		mean+=i*nHist[i];
+    	}
+    	return mean;
+    }
+
+    double variance(double[] nHist, double mean) {
+    	double var = 0;
+    	for(int i=0; i < nHist.length; i++) {
+    		var+=Math.pow(i-mean, 2)*nHist[i];
+    	}
+    	return var;
+    }
+    
+    double skewness(double[] nHist, double mean, double var) {
+    	double skew = 0;
+    	for(int i=0; i < nHist.length; i++) {
+    		skew+=Math.pow(i-mean,3)*nHist[i];
+    	}
+    	skew=Math.pow(Math.sqrt(var),3)*skew;
+    	return skew;
+    }
+    
+    double kurtosis(double[] nHist, double mean, double var) {
+    	double kur = 0;
+    	for(int i=0; i < nHist.length; i++) {
+    		mean+=Math.pow(i-mean, 4)*nHist[i];
+    	}
+    	kur = -3 + Math.pow(var, 2) * kur;
+    	return kur;
+    }
+
+
+	double entropy(double[] nHist) {
+		double entropy = 0;
+		for(int i=0; i < nHist.length; i++) {
+    		entropy+=nHist[i]*Math.log10(nHist[i]);
+    	}
+		return entropy;
+	}
+
 }
